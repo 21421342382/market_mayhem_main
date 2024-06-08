d2276fa1ede12af94d8c3167cde5b358b3547d0c
Harsh Jha <letscodeindia384@gmail.com>
2024-06-08 12:26:13 +0530

    error

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.json());
 
 // Enable CORS
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', ['http://localhost:3000','https://market-mayhem-client.onrender.com/']); // Allow requests from the dashboard
+    res.setHeader('Access-Control-Allow-Origin', ['http://localhost:3000','https://market-mayhem-client.onrender.com']); // Allow requests from the dashboard
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
